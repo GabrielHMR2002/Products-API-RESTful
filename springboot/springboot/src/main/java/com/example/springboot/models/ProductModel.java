596e3a31712bb50af6b03435cdd025d13dbb7af7
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-02 19:28:11 -0300

    Class ProductModel: Attributes, Getters and Setters

--- springboot/springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ springboot/springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,6 +1,8 @@
 package com.example.springboot.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -10,4 +12,33 @@ import java.io.Serializable;
 public class ProductModel implements Serializable {
 private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
