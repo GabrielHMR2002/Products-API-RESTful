41ff04e63fdfe6e10f4248746b454f6d067d8c40
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-02 21:18:53 -0300

    Method GET getOneProduct

--- springboot/springboot/src/main/java/com/example/springboot/controller/ProductController.java
+++ springboot/springboot/src/main/java/com/example/springboot/controller/ProductController.java
@@ -8,12 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -44,11 +43,24 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    // no No corpo vai ter uma lista de ProductModelo
+    //No corpo vai ter uma lista de ProductModelo
     public ResponseEntity<List<ProductModel>> getAllProducts(){
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
 
     }
 
+    /*<Object>: Isso indica que o corpo da resposta pode ser de qualquer tipo.
+     O Object é um tipo genérico, o que significa que você pode usar ResponseEntity para encapsular a resposta de diferentes tipos de objetos.*/
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+
+    }
 
 }
